948cfe7aca08011842f2cf7cca0d4460ba852564
space <space@space-juan>
2017-10-03 13:42:06 +0530

    error fix,arraylist for scenes,common back button template

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # android-app-OddOneOut
 choose odd one out from the screen shown.
+================================================
+ changes included
+================================================
+
+* Check current scene along with image id for evaluating the choice. 
+* Added common xml layout file for back button,which needs to be included in required layout files.
+* ArrayList for all scenes.
+* Generalized the method for back button with the help of arraylist.  
+
--- app/src/main/java/com/example/root/oddoneout/MainActivity.java
+++ app/src/main/java/com/example/root/oddoneout/MainActivity.java
@@ -7,9 +7,12 @@ import android.support.transition.Scene;
 import android.support.transition.Transition;
 import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.LinearInterpolator;
+
+import java.util.ArrayList;
 
 //import android.transition.TransitionInflater;
 
@@ -24,21 +27,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TransitionManager transitionMgr;
     private Transition transition;
 
+
     private MediaPlayer rightVoice, wrongVoice;
     private int currentScene;
 
+    ArrayList<Scene> list ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scene_transition);
 
+        list = new ArrayList<Scene>();
+
+
         //Preparing a root container
         rootContainer = (ViewGroup) findViewById(R.id.rootContainer);
+
 
         //referencing scenes created in xml
         scene1 = Scene.getSceneForLayout(rootContainer, R.layout.scene1_layout, this);
         scene2 = Scene.getSceneForLayout(rootContainer, R.layout.scene2_layout, this);
         scene3 = Scene.getSceneForLayout(rootContainer, R.layout.scene3_layout, this);
+
+        list.add(scene1);
+        list.add(scene2);
+        list.add(scene3);
 
         transition = new AutoTransition();
         transition.setDuration(500);
@@ -61,23 +74,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (view.getId()) {
             case R.id.imageButton_tux:
+               if(currentScene==1) {
 
-                changeToScene(scene2);
+                    changeToScene(scene2);
+                }
+                else {
+                    wrongVoice.start();
+                }
+                               
                 break;
 
             case R.id.imageButton_plant:
+                if(currentScene==2) {
 
-                changeToScene(scene3);
+                    changeToScene(scene3);
+                }
+                else {
+                    wrongVoice.start();
+                }
                 break;
 
-            case R.id.back_id2:
+            case R.id.imageButton_inback:
 
-                TransitionManager.go(scene1, transition);
-                break;
-
-            case R.id.back_id3:
-
-                TransitionManager.go(scene2, transition);
+                backToScene();
                 break;
             default:
                 wrongVoice.start();
@@ -87,43 +106,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-private void setCurrentScene(){
-    scene1.setEnterAction(new Runnable() {
-        @Override
-        public void run() {
-            currentScene = 1;
-        }
-    });
-    scene2.setEnterAction(new Runnable() {
-        @Override
-        public void run() {
-            currentScene = 2;
-        }
-    });
-    scene3.setEnterAction(new Runnable() {
-        @Override
-        public void run() {
-            currentScene = 3;
-        }
-    });
+    private void setCurrentScene() {
+        scene1.setEnterAction(new Runnable() {
+            @Override
+            public void run() {
+                currentScene = 1;
+            }
+        });
+        scene2.setEnterAction(new Runnable() {
+            @Override
+            public void run() {
+                currentScene = 2;
+            }
+        });
+        scene3.setEnterAction(new Runnable() {
+            @Override
+            public void run() {
+                currentScene = 3;
+            }
+        });
 
-}
-
-
-private void changeToScene( final Scene scene)   {
+    }
 
 
-    rightVoice.start();
-    rightVoice.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+    private void changeToScene(final Scene scene) {
 
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
 
-            TransitionManager.go(scene, transition);
-        }
+        rightVoice.start();
+        rightVoice.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
-    });
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
 
-}
+                TransitionManager.go(scene, transition);
+            }
+
+        });
+
+    }
+
+    private void backToScene() {
+
+
+        int index= currentScene-2;
+
+        Log.d("tagnnnn",Integer.toString(index));
+        TransitionManager.go(list.get(index),transition);
+
+
+    }
 
 }
